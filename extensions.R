f5042cc11e23234b4a34cb60a98dab3e81cc99cd
Stanimir Ivanov <stanimir.s.ivanov@gmail.com>
2018-06-04 17:23:15 +0200

    change ARIMAX to linear

--- extensions.R
+++ extensions.R
@@ -1,32 +1,25 @@
 load("./Data/macroeconomic_data_filtered.RData")
 
-ext <- lapply(loc_f, function(x) {
-  level <- arima(x$level, 
-                 order = c(0, 0, 1), 
-                 xreg = cbind(glob_f$level, 
-                             lag(x[,c("capacity.utilization", 
-                                      "inflation", 
-                                      "interest.rate", 
-                                      "unemployment")])
-                             )
-                 )
-  slope <- arima(x$slope, 
-                 order = c(0, 0, 1), 
-                 xreg = cbind(glob_f$slope,
-                              lag(x[,c("capacity.utilization", 
-                                       "inflation", 
-                                       "interest.rate", 
-                                       "unemployment")])
-                              )
-                 )
-  curvature <- arima(x$curvature, 
-                     order = c(0, 0, 1) , 
-                     xreg = cbind(glob_f$curvature,
-                                  lag(x[,c("capacity.utilization", 
-                                           "inflation", 
-                                           "interest.rate", 
-                                           "unemployment")])
-                                  )
-                     )
-  return(list("level" = level, "slope" = slope, "curvature" = curvature))
+ext1 <- lapply(loc_f, function(x) {
+
+  level <- lm(x$level ~ glob_f$level + lag(x$capacity.utilization) + 
+                lag(x$inflation) + lag(x$interest.rate) + stats::lag(x$unemployment))
+  level_ar1 <- lm(level$residuals ~ 0 + lag(level$residuals))
+  
+  slope <- lm(x$slope ~ glob_f$slope + lag(x$capacity.utilization) + 
+                lag(x$inflation) + lag(x$interest.rate) + lag(x$unemployment))
+  slope_ar1 <- lm(slope$residuals ~ 0 + lag(slope$residuals))
+  
+  curvature <- lm(x$curvature ~ glob_f$curvature + lag(x$capacity.utilization) + 
+                    lag(x$inflation) + lag(x$interest.rate) + lag(x$unemployment))
+  curvature_ar1 <- lm(curvature$residuals ~ 0 + lag(curvature$residuals))
+                     
+  return(list("level" = level, "slope" = slope, "curvature" = curvature,
+              "level_ar1" = level_ar1, "slope_ar1" = slope_ar1, "curvature_ar1" = curvature_ar1))
 })
+
+remove(glob_f)
+remove(loc_f)
+save.image("./Data/arimax_results.RData")
+
+# source("./LF Utils/generate_latent_factors.R")
--- factor_dynamics_replication.R
+++ factor_dynamics_replication.R
@@ -14,6 +14,20 @@ eq67 <- lapply(loc_f, function(x) {
   return(list("level" = level, "slope" = slope, "curvature" = curvature))
 })
 
+eq67alt <- lapply(loc_f, function(x) {
+  level <- lm(x$level ~ 1 + glob_f$level)
+  level_ar1 <- lm(level$residuals ~ 0 + lag(level$residuals))
+  
+  slope <- lm(x$slope ~ 1 + glob_f$slope)
+  slope_ar1 <- lm(slope$residuals ~ 0 + lag(slope$residuals))
+  
+  curvature <- lm(x$curvature ~ 1 + glob_f$curvature)
+  curvature_ar1 <- lm(curvature$residuals ~ 0 + lag(curvature$residuals))
+  
+  return(list("level" = level, "slope" = slope, "curvature" = curvature, 
+              "level_ar1" = level_ar1, "slope_ar1" = slope_ar1, "curvature_ar1" = curvature_ar1))
+})
+
 remove(glob_f)
 remove(loc_f)
 remove(loc_level)
