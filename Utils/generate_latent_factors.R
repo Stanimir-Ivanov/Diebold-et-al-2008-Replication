d6ff864207bb3b40b1e9e09b230a0ac272f3fe31
Stanimir Ivanov <stanimir.s.ivanov@gmail.com>
2018-06-12 12:30:06 +0200

    Panel data extension

--- Utils/generate_latent_factors.R
+++ Utils/generate_latent_factors.R
@@ -21,14 +21,43 @@ get_lf_ma1 <- function(yc_res_data, tau, lambda) {
     }))
 }
 
+get_pd <- function(pandel_data, tau, lambda) {
+  return(rollapply(pandel_data, width = 1, by.column = FALSE, FUN = function(x){
+    rate <- x[,grepl("Yield.Curve", colnames(x))]
+    fit <- x[,grepl("Yield.Curve.Fit", colnames(x))]
+    fit_res <- x[,grepl("Residuals", colnames(x))]
+    res <- Panel.Data.Heterogen(rate, fit, fit_res, tau)
+    return(res)
+  }))
+}
+
 get_res <- function(yield_curve, lambda) {
   return(
     rollapply(yield_curve, width = 1, by.column = FALSE, FUN = function(x) {
       res <- Nelson.Siegel(rate = x, tau = colnames(x) %>% as.numeric(), lambda = lambda)
-      colnames(res$Res) <- paste(colnames(res$Res),"Residuals")
+      # colnames(res$Res) <- paste(colnames(res$Res),"Residuals")
       return(res$Res)
     })
   )
+}
+
+get_fit <- function(yield_curve, lambda) {
+  return(
+    rollapply(yield_curve, width = 1, by.column = FALSE, FUN = function(x) {
+      fit <- Nelson.Siegel.Fit(rate = x, tau = colnames(x) %>% as.numeric(), lambda = lambda)
+      return(fit)
+    })
+  )
+}
+
+Panel.Data.Heterogen <- function(rate, fit, fit_res, tau) {
+  t <- time(rate)
+  res <- lm(t(rate) ~ 0 + t(fit) + f(fit_res) + factor(tau) - 1)
+  betaPar <- coef(beta) %>% t() %>% as.xts(order.by = t)
+  names(betaPar) <- c("fit", "residual")
+  fit <- fitted.values(beta) %>% t() %>% as.xts(order.by = t)
+  EstResults <- list(Par=betaPar, Fit=fit)
+  return(EstResults)
 }
 
 Nelson.Siegel <- function(rate, tau, lambda) {
@@ -59,6 +88,17 @@ Nelson.Siegel.MA1 <- function(rate, rate_residuals, tau, lambda) {
   return(EstResults)
 }
 
+Nelson.Siegel.Fit <- function(rate, tau, lambda) {
+  t <- time(rate)
+  fb1 <- factorBeta1(tau, lambda) %>% t() %>% as.xts(order.by = t)
+  fb2 <- factorBeta2(tau, lambda) %>% t() %>% as.xts(order.by = t)
+  
+  beta <- lm(t(rate) ~ 1 + t(fb1) + t(fb2))
+  fit <- fitted.values(beta) %>% t() %>% as.xts(order.by = t)
+  names(fit) <- tau
+  return(fit)
+}
+
 Nelson.Siegel2 <- function(rate, tau, lambda) {
   t <- time(rate)
   fb1 <- factorBeta1(tau, lambda) %>% t() %>% as.xts(order.by = t)
@@ -79,4 +119,11 @@ factorBeta1 <- function(tau, lambda) {
 
 factorBeta2 <- function(tau, lambda) {
   return((1 - exp(-lambda * tau))/(lambda * tau) - exp(-lambda * tau))
+}
+
+melt_list <- function(lst) {
+  return(lapply(lst, function(x){
+    tmp <- data.frame(time(x), x)
+    return(melt(tmp, id = colnames(tmp)[grepl("time", colnames(tmp))]))
+  }))
 }
--- extension_2.R
+++ extension_2.R
@@ -2,6 +2,10 @@ load("./Data/grouped_yield_curves.RData")
 source("./Utils/generate_latent_factors.R")
 source("range.R")
 
+fit <- lapply(yield_curves, function(x){
+  return(get_fit(x, lambda))
+})
+
 res <- lapply(yield_curves, function(x){
   return(get_res(yield_curve = x, lambda = lambda) %>% stats::lag())
 })
@@ -15,14 +19,30 @@ yield_curves <- lapply(yield_curves, function(x){
   return(x[2:length(time(x)),])
 })
 
-ext2 <- mapply(FUN = function(x, y){
-  tau <- colnames(x) %>% as.numeric()
-  colnames(x) = paste(colnames(x), "Yield Curve")
-  yc_res_data <- cbind(x, y)
-  return(get_lf_ma1(yc_res_data, tau, lambda))
-}, x = yield_curves, y = res, SIMPLIFY = FALSE)
+melted_yc <- melt_list(yield_curves)
+melted_fit <- melt_list(fit)
+melted_res <- melt_list(res)
 
-remove(yield_curves)
-remove(res)
+panel_data <- mapply(FUN = function(x, y, z){
+  tmp <- merge(x, y, by = c("time.x.", "variable"))
+  tmp <- merge(tmp, z, by = c("time.x.", "variable"))
+  colnames(tmp) <- c("time", "maturity", "yield", "fit", "res")
+  return(tmp)
+}, x = melted_yc, y = melted_fit, z = melted_res, SIMPLIFY = FALSE)
+
+# ext2 <- mapply(FUN = function(x, y){
+#   tau <- colnames(x) %>% as.numeric()
+#   colnames(x) = paste(colnames(x), "Yield Curve")
+#   yc_res_data <- cbind(x, y)
+#   return(get_lf_ma1(yc_res_data, tau, lambda))
+# }, x = yield_curves, y = res, SIMPLIFY = FALSE)
+
+ext2_pd <- lapply(panel_data, function(x){
+  beta <- lm(yield ~ fit + res + factor(maturity), data = x)
+  return(beta)
+})
+
+remove(yield_curves, res, fit)
+remove(melted_yc, melted_res, melted_fit, panel_data)
 
 save.image("./Data/extention2_results.RData")
