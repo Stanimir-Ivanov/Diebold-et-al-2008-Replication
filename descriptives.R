a0761b964ec52652ffb9daf4c22b45d78c0cc831
Stanimir Ivanov <stanimir.s.ivanov@gmail.com>
2018-06-30 14:02:05 +0200

    Finished plots and tables without forecasting

--- descriptives.R
+++ descriptives.R
@@ -25,3 +25,34 @@ eq67data <- lapply(eq67, function(x){
     return(res)
   }))
 })
+
+ext1altdata <- lapply(ext1alt, function(x){
+  return(lapply(x, function(y){
+    res <- rbind(c(y$coef, sqrt(y$sigma2)), c(diag(sqrt(y$var.coef)), NaN))
+    colnames(res) <- c(names(y$coef), "sigma")
+    return(res)
+  }))
+})
+
+ext2data <- lapply(ext2_pd, function(x){
+  return(summary(x))
+})
+
+alphas <- lapply(ext2_pd, function(x) {
+  return(x$coefficients[grepl("factor", names(x$coefficients))])
+})
+
+library(latex2exp)
+
+df <- data.frame(alphas$US, alphas$CA, alphas$JP, alphas$DE, alphas$UK)
+colnames(df) <- c("US", "CA", "JP", "DE", "UK")
+tau <- c(6, 9 ,12, 15, 18, 21, 24, 30, 36, 48, 60, 72, 84, 96, 108, 120)
+rownames(df) <- tau
+dfz <- as.zoo(df)
+
+plot.zoo(dfz, plot.type = "single", axes = FALSE, col = 1:5, type = 'l', ylab = TeX("$\\alpha$"),
+         xlab = TeX("$\\tau$"))
+axis(side = 1, at = seq(1, 16, by = 3), labels = tau[seq(1, 16, by = 3)])
+axis(side = 2)
+legend("topleft", lty = 1, col = 1:5, colnames(dfz), cex = .6)
+
---
+++ plotting.R
@@ -0,0 +1,54 @@
+par(mfrow=c(1,1))
+plot.xts(glob_f, main = "Global Latent Factors", major.ticks.on = "years", grid.ticks.on = "years", grid.ticks.lwd = 1, grid.ticks.lty = 1, lwd = 1.5)
+title(ylab="Principal Component")
+axis(side = 1, outer = TRUE, at = seq(1, 273, by = 36), labels = format(time(glob_f$level)[seq(1, 273, by = 36)], "%Y"), tck = 36)
+
+par(mfrow=c(3,1))
+plot.xts(
+  x = cbind(
+    loc_f$US$level,
+    loc_f$CA$level,
+    loc_f$DE$level,
+    loc_f$JP$level,
+    loc_f$UK$level
+  ),
+  main = "Local Level Factors",
+  lwd = 1.5,
+  yaxis.right = FALSE,
+  major.ticks.on = "years", grid.ticks.on = "years",
+  grid 	
+)
+
+axis(side = 1, outer = TRUE, at = seq(1, 273, by = 36), labels = format(time(glob_f$level)[seq(1, 273, by = 36)], "%Y"), tck = 36)
+
+plot.xts(
+  x = cbind(
+    loc_f$US$slope,
+    loc_f$CA$slope,
+    loc_f$DE$slope,
+    loc_f$JP$slope,
+    loc_f$UK$slope
+  ),
+  main = "Local Slope Factors",
+  lwd = 1.5,
+  yaxis.right = FALSE,
+  major.ticks.on = "years", grid.ticks.on = "years",
+)
+
+axis(side = 1, outer = TRUE, at = seq(1, 273, by = 36), labels = format(time(glob_f$level)[seq(1, 273, by = 36)], "%Y"), tck = 36)
+
+plot.xts(
+  x = cbind(
+    loc_f$US$curvature,
+    loc_f$CA$curvature,
+    loc_f$DE$curvature,
+    loc_f$JP$curvature,
+    loc_f$UK$curvature
+  ),
+  main = "Local Curvature Factors",
+  lwd = 1.5,
+  yaxis.right = FALSE,
+  major.ticks.on = "years", grid.ticks.on = "years",
+)
+
+axis(side = 1, outer = TRUE, at = seq(1, 273, by = 36), labels = format(time(glob_f$level)[seq(1, 273, by = 36)], "%Y"), tck = 36)
